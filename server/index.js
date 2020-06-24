2fea4e4a102b79c7f685eb879e334653b4997567
Tim <timyee90@gmail.com>
2020-06-24 14:01:00 -0400

    add cluster to server

--- server/index.js
+++ server/index.js
@@ -1,20 +1,35 @@
-const express = require('express');
-const morgan = require('morgan');
-const compression = require('compression');
-const path = require('path');
-const router = require('./router.js');
-const app = express();
+const cluster = require('cluster');
+const os = require('os');
+const useCluster = true;
 
-app.use(morgan('dev'));
-app.use(compression());
-app.use((req, res, next) => {
-  res.set('Cache-Control', 'no-store');
-  next();
-});
+if (useCluster && cluster.isMaster) {
+  const n_cpus = os.cpus().length;
+  console.log(`Forking ${n_cpus} CPUS`);
+  for (let i = 0; i < n_cpus; i++) {
+    cluster.fork();
+  }
+} else {
+  const express = require('express');
+  const morgan = require('morgan');
+  const compression = require('compression');
+  const path = require('path');
+  const router = require('./router.js');
+  const app = express();
+  const pid = process.pid;
 
-app.use(express.static(path.join(__dirname, '../client/')));
-app.use('/', router);
+  const port = process.env.PORT || 3000;
 
-const port = process.env.PORT || 3000;
+  app.listen(port, () =>
+    console.log(`Server process: ${pid} is listening of port: ${port}`)
+  );
 
-app.listen(port, () => console.log(`Listening of port: ${port}`));
+  app.use(morgan('dev'));
+  app.use(compression());
+  app.use((req, res, next) => {
+    res.set('Cache-Control', 'no-store');
+    next();
+  });
+
+  app.use(express.static(path.join(__dirname, '../client/')));
+  app.use('/', router);
+}
